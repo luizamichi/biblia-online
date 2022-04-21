aa4caf9d09b582dcb382204cd1f3e0f2b20863f9
Luiz Amichi <luizamichi@gmail.com>
2022-04-21 18:33:43 -0300

    Auto carregamento das classes

---
+++ autoload.php
@@ -0,0 +1,33 @@
+<?php
+
+// Cadastra o autoload de todas as classes do sistema
+spl_autoload_register(function(string $classe): void {
+	$diretorios = ["controllers", "daos", "models"];
+	foreach($diretorios as $diretorio) {
+		$arquivo = __DIR__ . "/" . $diretorio . "/" . $classe . ".php";
+		if(file_exists($arquivo)) {
+			require_once($arquivo);
+			break;
+		}
+	}
+});
+
+
+// Ativa a sessão do PHP
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_name(Configuracao::ini()::get("session_name", "project"));
+	session_start();
+	session_regenerate_id();
+}
+
+// Ativa a depuração de erros
+if(Configuracao::ini()::get("debug", "project")) {
+	ini_set("display_errors", 1);
+	ini_set("display_startup_errors", 1);
+	error_reporting(E_ALL);
+}
+else {
+	ini_set("display_errors", 0);
+	ini_set("display_startup_errors", 0);
+	error_reporting(~E_ALL);
+}
