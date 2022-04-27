3f21d0637bc2aa36c27b2c114923a806c861230b
Luiz Amichi <luizamichi@gmail.com>
2022-04-27 09:13:37 -0300

    Classes para versículo e conjunto de versículos

---
+++ models/Capitulo.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once(__DIR__ . "/../autoload.php");
+
+
+/**
+ * Classe entidade
+ */
+class Capitulo extends Modelo {
+	/**
+	 * @var int $numero
+	 * @var array[Versiculo] $versiculos
+	 */
+	private int $numero;
+	private array $versiculos;
+
+	/**
+	 * @param int $numero
+	 * @param array[Versiculo] $versiculos
+	 * @return void
+	 */
+	public function __construct(int $numero=0, array $versiculos=[]) {
+		$this->numero = $numero;
+		$this->versiculos = $versiculos;
+
+		parent::__construct($this);
+	}
+
+
+	/**
+	 * @param string $atributo
+	 * @param array[Versiculo]|int $valor
+	 * @return void
+	 */
+	public function __set(string $atributo, array|int $valor): void {
+		if(in_array($atributo, ["numero", "versiculos"])) {
+			$this->$atributo = $valor;
+		}
+	}
+
+
+	/**
+	 * @param string $atributo
+	 * @return ?array[Versiculo]|?int
+	 */
+	public function __get(string $atributo): array|int|null {
+		return $this->$atributo ?? null;
+	}
+}
---
+++ models/Versiculo.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once(__DIR__ . "/../autoload.php");
+
+
+/**
+ * Classe entidade
+ */
+class Versiculo extends Modelo {
+	/**
+	 * @var int $chave
+	 * @var Versao $versao
+	 * @var Livro $livro
+	 * @var int $capitulo
+	 * @var int $numero
+	 * @var string $texto
+	 */
+	private int $chave;
+	private Versao $versao;
+	private Livro $livro;
+	private int $capitulo;
+	private int $numero;
+	private string $texto;
+
+	/**
+	 * @var bool TABELA
+	 */
+	public const TABELA = true;
+
+	/**
+	 * @param int $chave
+	 * @param Versao $versao
+	 * @param Livro $livro
+	 * @param int $capitulo
+	 * @param int $numero
+	 * @param string $texto
+	 * @return void
+	 */
+	public function __construct(int $chave=0, Versao $versao=null, Livro $livro=null, int $capitulo=0, int $numero=0, string $texto="") {
+		$this->chave = $chave;
+		$this->versao = $versao ?? (new Versao);
+		$this->livro = $livro ?? (new Livro);
+		$this->capitulo = $capitulo;
+		$this->numero = $numero;
+		$this->texto = $texto;
+		parent::__construct($this);
+	}
+
+
+	/**
+	 * @param string $atributo
+	 * @param int|Livro|string|Versao $valor
+	 * @return void
+	 */
+	public function __set(string $atributo, int|Livro|string|Versao $valor): void {
+		if(in_array($atributo, ["chave", "versao", "livro", "capitulo", "numero", "texto"])) {
+			$this->$atributo = $valor;
+		}
+	}
+
+
+	/**
+	 * @param string $atributo
+	 * @return ?int|?Livro|?string|?Versao
+	 */
+	public function __get(string $atributo): array|int|Livro|null|string|Versao {
+		return $this->$atributo ?? null;
+	}
+}
