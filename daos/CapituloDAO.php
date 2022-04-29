5cf66e7dab7b36d31c45e19f32c6fae5076c5e19
Luiz Amichi <luizamichi@gmail.com>
2022-04-28 22:49:11 -0300

    DAO de capítulo, livro e versículo

---
+++ daos/CapituloDAO.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once(__DIR__ . "/../autoload.php");
+
+
+/**
+ * Classe complementar de capítulos
+ * @author Luiz Amichi <luizamichi@luizamichi.com>
+ */
+class CapituloDAO {
+	/**
+	 * @static
+	 * @param int $numero
+	 * @param string $livroAbreviado
+	 * @param string $versaoAbreviado
+	 * @return Capitulo
+	 */
+	public static function numeroLivroVersao(int $numero, string $livroAbreviado, string $versaoAbreviado): Capitulo {
+		$livro = LivroDAO::abreviado($livroAbreviado);
+		$versao = VersaoDAO::abreviado($versaoAbreviado);
+		$capitulo = new Capitulo($numero);
+
+		$capitulo->versiculos = VersiculoDAO::versaoLivroCapitulo(
+			$versao->chave, $livro->chave, $capitulo->numero
+		) ?: [new Versiculo(0, $versao, $livro, $numero)];
+
+		return $capitulo;
+	}
+
+
+	/**
+	 * @static
+	 * @param Capitulo $capitulo
+	 * @return bool
+	 */
+	public static function update(Capitulo $capitulo): bool {
+		$consulta = "";
+		$parametros = [];
+
+		foreach($capitulo->versiculos as $versiculo) {
+			$consulta .= "UPDATE `versiculos` SET `versiculo_versao_id` = :versiculo_versao_id" . $versiculo->chave . ", `versiculo_livro_id` = :versiculo_livro_id" . $versiculo->chave . ", `versiculo_capitulo` = :versiculo_capitulo" . $versiculo->chave . ", `versiculo_numero` = :versiculo_numero" . $versiculo->chave . ", `versiculo_texto` = :versiculo_texto" . $versiculo->chave . " WHERE `versiculo_id` = :versiculo_id" . $versiculo->chave . ";";
+			$parametros[] = [
+				[":versiculo_versao_id" . $versiculo->chave, $versiculo->versao->chave, PDO::PARAM_INT],
+				[":versiculo_livro_id" . $versiculo->chave, $versiculo->livro->chave, PDO::PARAM_INT],
+				[":versiculo_capitulo" . $versiculo->chave, $versiculo->capitulo, PDO::PARAM_INT],
+				[":versiculo_numero" . $versiculo->chave, $versiculo->numero, PDO::PARAM_INT],
+				[":versiculo_texto" . $versiculo->chave, $versiculo->texto, PDO::PARAM_STR],
+				[":versiculo_id" . $versiculo->chave, $versiculo->chave, PDO::PARAM_INT]
+			];
+		}
+
+		return VersiculoDAO::postAll($consulta, $parametros);
+	}
+}
---
+++ daos/VersiculoDAO.php
@@ -0,0 +1,132 @@
+<?php
+
+require_once(__DIR__ . "/../autoload.php");
+
+
+/**
+ * Classe ORM de versículos
+ * @author Luiz Amichi <luizamichi@luizamichi.com>
+ */
+class VersiculoDAO extends DAO {
+	/**
+	 * @static
+	 * @param object $linha
+	 * @return Versiculo
+	 */
+	public static function associative(object $linha): Versiculo {
+		return new Versiculo(
+			$linha->versiculo_id,
+			VersaoDAO::chave($linha->versiculo_versao_id),
+			LivroDAO::chave($linha->versiculo_livro_id),
+			$linha->versiculo_capitulo,
+			$linha->versiculo_numero,
+			$linha->versiculo_texto
+		);
+	}
+
+
+	/**
+	 * @static
+	 * @param int $chave
+	 * @return ?Versiculo
+	 */
+	public static function chave(int $chave): ?Versiculo {
+		$consulta = "SELECT `versiculo_id`, `versiculo_versao_id`, `versiculo_livro_id`, `versiculo_capitulo`, `versiculo_numero`, `versiculo_texto` FROM `versiculos` WHERE `versiculo_id` = :versiculo_id;";
+		$parametros = [
+			[":versiculo_id", $chave, PDO::PARAM_INT]
+		];
+
+		return parent::fetch($consulta, $parametros, function(object $linha): Versiculo {
+			return self::associative($linha);
+		});
+	}
+
+
+	/**
+	 * @static
+	 * @param int $versao
+	 * @param int $livro
+	 * @param int $capitulo
+	 * @return array
+	 */
+	public static function versaoLivroCapitulo(int $versao, int $livro, int $capitulo): array {
+		$consulta = "SELECT `versiculo_id`, `versiculo_versao_id`, `versiculo_livro_id`, `versiculo_capitulo`, `versiculo_numero`, `versiculo_texto` FROM `versiculos` WHERE `versiculo_versao_id` = :versiculo_versao_id AND `versiculo_livro_id` = :versiculo_livro_id AND `versiculo_capitulo` = :versiculo_capitulo;";
+		$parametros = [
+			[":versiculo_versao_id", $versao, PDO::PARAM_INT],
+			[":versiculo_livro_id", $livro, PDO::PARAM_INT],
+			[":versiculo_capitulo", $capitulo, PDO::PARAM_INT]
+		];
+
+		return parent::fetchAll($consulta, $parametros, function(object $linha): Versiculo {
+			return self::associative($linha);
+		});
+	}
+
+
+	/**
+	 * @static
+	 * @param int $versao
+	 * @param int $livro
+	 * @param int $capitulo
+	 * @param int $numero
+	 * @return ?Versiculo
+	 */
+	public static function versaoLivroCapituloNumero(int $versao, int $livro, int $capitulo, int $numero): ?Versiculo {
+		$consulta = "SELECT `versiculo_id`, `versiculo_versao_id`, `versiculo_livro_id`, `versiculo_capitulo`, `versiculo_numero`, `versiculo_texto` FROM `versiculos` WHERE `versiculo_versao_id` = :versiculo_versao_id AND `versiculo_livro_id` = :versiculo_livro_id AND `versiculo_capitulo` = :versiculo_capitulo AND `versiculo_numero` = :versiculo_numero;";
+		$parametros = [
+			[":versiculo_versao_id", $versao, PDO::PARAM_INT],
+			[":versiculo_livro_id", $livro, PDO::PARAM_INT],
+			[":versiculo_capitulo", $capitulo, PDO::PARAM_INT],
+			[":versiculo_numero", $numero, PDO::PARAM_INT]
+		];
+
+		return parent::fetch($consulta, $parametros, function(object $linha): Versiculo {
+			return self::associative($linha);
+		});
+	}
+
+
+	/**
+	 * @static
+	 * @return int
+	 */
+	public static function length(): int {
+		$consulta = "SELECT count(*) tamanho FROM `versiculos`;";
+		return parent::fetch($consulta)->tamanho ?? 0;
+	}
+
+
+	/**
+	 * @static
+	 * @param Versiculo $versiculo
+	 * @return bool
+	 */
+	public static function update(Versiculo $versiculo): bool {
+		$consulta = "UPDATE `versiculos` SET `versiculo_versao_id` = :versiculo_versao_id, `versiculo_livro_id` = :versiculo_livro_id, `versiculo_capitulo` = :versiculo_capitulo, `versiculo_numero` = :versiculo_numero, `versiculo_texto` = :versiculo_texto WHERE `versiculo_id` = :versiculo_id;";
+		$parametros = [
+			[":versiculo_versao_id", $versiculo->versao->chave, PDO::PARAM_INT],
+			[":versiculo_livro_id", $versiculo->livro->chave, PDO::PARAM_INT],
+			[":versiculo_capitulo", $versiculo->capitulo, PDO::PARAM_INT],
+			[":versiculo_numero", $versiculo->numero, PDO::PARAM_INT],
+			[":versiculo_texto", $versiculo->texto, PDO::PARAM_STR],
+			[":versiculo_id", $versiculo->chave, PDO::PARAM_INT]
+		];
+
+		return parent::post($consulta, $parametros);
+	}
+
+
+	/**
+	 * @static
+	 * @param int
+	 * @return bool
+	 */
+	public static function delete(int $chave): bool {
+		$consulta = "DELETE FROM `versiculos` WHERE `versiculo_id` = :versiculo_id;";
+		$parametros = [
+			[":versiculo_id", $chave, PDO::PARAM_INT]
+		];
+
+		return parent::post($consulta, $parametros);
+	}
+}
