5470ab4614997e5985929173b0f8284de6c47889
Luiz Amichi <luizamichi@gmail.com>
2022-07-15 19:15:00 -0300

    Gerenciamento de rotas

--- controllers/RotaController.php
+++ controllers/RotaController.php
@@ -28,6 +28,208 @@ class RotaController {
 
 
 	/**
+	 * @static
+	 * @return void
+	 */
+	public static function home(): void {
+		$template = new Template("home");
+		$template->titulo = Configuracao::ini()::getStr("system_name", "project");
+
+		$tamanho = VersiculoDAO::length();
+		$tamanho = max(1, $tamanho);
+		$template->versiculoAleatorio = VersiculoDAO::chave(mt_rand(1, $tamanho));
+
+		mt_srand((int) date("dmY"));
+		$template->versiculoDiario = VersiculoDAO::chave(mt_rand(1, $tamanho));
+
+		mt_srand((int) date("WmY"));
+		$template->versiculoSemanal = VersiculoDAO::chave(mt_rand(1, $tamanho));
+
+		mt_srand((int) date("mY"));
+		$template->versiculoMensal = VersiculoDAO::chave(mt_rand(1, $tamanho));
+		$template->body();
+	}
+
+
+	/**
+	 * @param string $apelido
+	 *
+	 * @static
+	 * @return void
+	 */
+	public static function autor(string $apelido): void {
+		$autor = AutorDAO::apelido($apelido);
+		$livros = LivroDAO::autor((int) $autor?->chave);
+
+		$template = new Template("autor");
+		$template->autor = $autor;
+		$template->livros = $livros;
+		$template->body();
+	}
+
+
+	/**
+	 * @static
+	 * @return void
+	 */
+	public static function autores(): void {
+		$autores = AutorDAO::all();
+
+		$template = new Template("autores");
+		$template->autores = $autores;
+		$template->body();
+	}
+
+
+	/**
+	 * @param string $nome
+	 *
+	 * @static
+	 * @return void
+	 */
+	public static function livro(string $nome): void {
+		$livro = LivroDAO::abreviado($nome);
+		$autores = AutorDAO::livro((int) $livro?->chave);
+
+		$template = new Template("livro");
+		$template->livro = $livro;
+		$template->autores = $autores;
+		$template->body();
+	}
+
+
+	/**
+	 * @param string $testamento
+	 *
+	 * @static
+	 * @return bool
+	 */
+	public static function livros(string $testamento=""): bool {
+		$testamento = strtoupper($testamento);
+
+		if(in_array($testamento, Sessao::testamentos())) {
+			$testamento = TestamentoDAO::abreviado($testamento);
+			self::testamento($testamento ?? new Testamento);
+			return false;
+		}
+		else {
+			$livros = LivroDAO::all();
+		}
+
+		$template = new Template("livros");
+		$template->livros = $livros;
+		$template->body();
+
+		return true;
+	}
+
+
+	/**
+	 * @param string $nome
+	 * @param string $versao
+	 * @param int $capitulo
+	 *
+	 * @static
+	 * @return void
+	 */
+	public static function capitulo(string $nome, string $versao, int $capitulo): void {
+		$capitulo = CapituloDAO::numeroLivroVersao($capitulo, $nome, $versao);
+
+		$template = new Template("capitulo");
+		$template->capitulo = $capitulo;
+		$template->body();
+	}
+
+
+	/**
+	 * @param string $versao
+	 * @param string $livro
+	 * @param int $capitulo
+	 * @param int $numero
+	 *
+	 * @static
+	 * @return void
+	 */
+	public static function versiculo(string $versao, string $livro, int $capitulo, int $numero): void {
+		$capituloModel = CapituloDAO::numeroLivroVersao($capitulo, $livro, $versao);
+		$versiculo = VersiculoDAO::versaoLivroCapituloNumero(
+			$capituloModel->versiculos[0]->versao->chave ?? 0,
+			$capituloModel->versiculos[0]->livro->chave ?? 0,
+			$capitulo,
+			$numero
+		)
+		?: new Versiculo(
+			0,
+			$capituloModel->versiculos[0]->versao ?? null,
+			$capituloModel->versiculos[0]->livro ?? null,
+			$capitulo,
+			$numero
+		);
+
+		$template = new Template("versiculo");
+		$template->versiculo = $versiculo;
+		$template->body();
+	}
+
+
+	/**
+	 * @param Testamento $testamento
+	 *
+	 * @static
+	 * @return void
+	 */
+	public static function testamento(Testamento $testamento): void {
+		$livros = LivroDAO::testamento($testamento->chave);
+
+		$template = new Template("testamento");
+		$template->testamento = $testamento;
+		$template->livros = $livros;
+		$template->body();
+	}
+
+
+	/**
+	 * @static
+	 * @return void
+	 */
+	public static function testamentos(): void {
+		$testamentos = TestamentoDAO::all();
+
+		$template = new Template("testamentos");
+		$template->testamentos = $testamentos;
+		$template->body();
+	}
+
+
+	/**
+	 * @param string $abreviado
+	 *
+	 * @static
+	 * @return void
+	 */
+	public static function versao(string $abreviado): void {
+		$versao = VersaoDAO::abreviado($abreviado);
+
+		$template = new Template("versao");
+		$template->versao = $versao;
+		$template->body();
+	}
+
+
+	/**
+	 * @static
+	 * @return void
+	 */
+	public static function versoes(): void {
+		$versoes = VersaoDAO::all();
+
+		$template = new Template("versoes");
+		$template->versoes = $versoes;
+		$template->body();
+	}
+
+
+	/**
 	 * @param ?Throwable $excecao
 	 *
 	 * @static
--- rotas.php
+++ rotas.php
@@ -27,6 +27,40 @@ if(is_file($arquivo) && !in_array(substr($requisicao, 1), (array) Configuracao::
 	exit;
 }
 
+try {
+	Conexao::get();
+}
+catch(Throwable $th) {
+	http_response_code(500);
+
+	if($direcionador === "api") {
+		header("Content-Type: application/json");
+		exit(json_encode([
+			"resultado" => null,
+			"mensagem" => $th->getMessage(),
+			"erro" => ErroController::last(),
+			"sucesso" => false
+		]));
+	}
+	else {
+		RotaController::erro($th);
+		exit;
+	}
+}
+
+// Carrega os valores da sessão (para evitar consulta ao banco de dados)
+$autores = Sessao::autores();
+$livros = Sessao::livros();
+$versoes = Sessao::versoes();
+
+// Foi informada a versão desejada
+if(in_array(strtoupper($parametros[0] ?? ""), $versoes)) {
+	$versao = strtoupper($parametros[0]);
+	$direcionador = $parametros[1] ?? "";
+	$capitulo = (int) ($parametros[2] ?? 0);
+	$versiculo = (int) ($parametros[3] ?? 0);
+}
+
 define("VERSAO", $versao . "/");
 
 switch($direcionador) {
@@ -37,5 +71,49 @@ switch($direcionador) {
 	// Rota específica para login/logout
 	case "login":
 		RotaController::login();
+		break;
+	// Exibe todos os autores
+	case "autores":
+		RotaController::autores();
+		break;
+	// Exibe todos os livros
+	case "livros":
+		RotaController::livros($parametros[2] ?? "");
+		break;
+	// Exibe todos os testamentos
+	case "testamentos":
+		RotaController::testamentos();
+		break;
+	// Exibe todas as versões
+	case "versoes":
+		RotaController::versoes();
+		break;
+	default:
+		// Exibe um autor em específico
+		if(in_array(strtoupper($direcionador), $autores)) {
+			RotaController::autor($direcionador);
+		}
+		elseif(in_array(strtoupper($direcionador), $livros)) {
+			// Exibe um versículo específico
+			if($versiculo !== 0) {
+				RotaController::versiculo($versao, strtoupper($direcionador), $capitulo, $versiculo);
+			}
+			// Exibe um capítulo específico
+			elseif($capitulo !== 0) {
+				RotaController::capitulo(strtoupper($direcionador), $versao, $capitulo);
+			}
+			// Exibe um livro específico
+			else {
+				RotaController::livro(strtoupper($direcionador));
+			}
+		}
+		else {
+			if(Operador::logged()) {
+				RotaController::versao($versao);
+			}
+			else {
+				RotaController::home();
+			}
+		}
 		break;
 }
