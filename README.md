e6979ac8f473d14244659f64766d97510cd51f65
Luiz Amichi <luizamichi@gmail.com>
2022-07-20 20:00:00 -0300

    Gerenciamento de rotas

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Bíblia Online
+
+Este é um site em PHP desenvolvido com a utilização do MySQL para armazenamento de dados, com o objetivo de fornecer acesso fácil e rápido aos versículos da Bíblia, livre de anúncios presentes em diversos sites da web que oferecem a mesma funcionalidade.
+
+## Configurações
+
+### Arquivo de configuração (biblia.ini)
+
+- O arquivo `biblia.ini` contém diversas configurações essenciais para o funcionamento do sistema, incluindo:
+  - Configurações de comunicação com o SGBD;
+  - Detalhes de autenticação de usuários;
+  - Definições de permissões de operações;
+  - Opção de ativar a depuração para exibir erros em tela;
+  - Possibilidade de registro de erros em arquivo quando a depuração está desativada;
+  - Restrições de quais arquivos podem ser acessados;
+  - Nome do COOKIE de sessão;
+  - Título e descrição do sistema.
+
+### Arquivo de configuração de URL (`.htaccess`)
+
+- No arquivo `.htaccess`, você deve configurar a URL base do sistema para garantir o funcionamento correto das rotas.
+
+## Requisitos do sistema
+
+- [MySQL](https://www.mysql.com): 8.0
+- [PHP](https://www.php.net): 8.1
+
+## Frameworks utilizados
+
+- [jQuery](https://jquery.com): 3.7.1
+- [Semantic UI](https://semantic-ui.com): 2.5.0
+
+## Testes locais
+
+Para executar o sistema localmente para fins de teste, utilize o seguinte comando:
+
+```bash
+php -S localhost:8080 rotas.php
+```
+
+Certifique-se de que todas as dependências estejam devidamente instaladas e configuradas antes de iniciar os testes.
+
+Aproveite o acesso fácil aos versículos da Bíblia e o ambiente livre de anúncios fornecido pelo projeto [Bíblia Online](http://biblia.luizamichi.com.br)!
--- rotas.php
+++ rotas.php
@@ -6,6 +6,18 @@ $requisicao = (string) strtok($_SERVER["REQUEST_URI"] ?? "", "?");
 $parametros = array_values(array_filter(explode("/", $requisicao)));
 $direcionador = $parametros[0] ?? "";
 
+// Versão padrão
+$versao = "NVI";
+$capitulo = 0;
+$versiculo = 0;
+
+// Carrega os arquivos estáticos antes de tentar se comunicar com o banco de dados
+$arquivo = __DIR__ . $requisicao;
+if(is_file($arquivo) && !in_array(substr($requisicao, 1), (array) Configuracao::ini()::get("restrict"))) {
+	RotaController::files($arquivo);
+	exit;
+}
+
 try {
 	Conexao::get();
 }
@@ -22,17 +34,77 @@ catch(Throwable $th) {
 		]));
 	}
 	else {
+		RotaController::erro();
 		exit;
 	}
 }
 
+// Carrega os valores da sessão (para evitar consulta ao banco de dados)
+$autores = Sessao::autores();
+$livros = Sessao::livros();
+$versoes = Sessao::versoes();
+
+// Foi informada a versão desejada
+if(in_array(strtoupper($parametros[0] ?? ""), $versoes)) {
+	$versao = strtoupper($parametros[0]);
+	$direcionador = $parametros[1] ?? "";
+	$capitulo = (int) ($parametros[2] ?? 0);
+	$versiculo = (int) ($parametros[3] ?? 0);
+}
+
+define("VERSAO", $versao . "/");
+
 switch($direcionador) {
 	// Realiza CRUD no banco de dados
 	case "api":
-		include_once __DIR__ . "/api.php";
+		RotaController::api();
 		break;
 	// Rota específica para login/logout
 	case "login":
-		include_once __DIR__ . "/login.php";
+		RotaController::login();
+		break;
+	// Exibe todos os autores
+	case "autores":
+		RotaController::autores();
+		break;
+	// Exibe todos os livros
+	case "livros":
+		RotaController::livros($parametros[2] ?? "");
+		break;
+	// Exibe todos os testamentos
+	case "testamentos":
+		RotaController::testamentos();
+		break;
+	// Exibe todas as versões
+	case "versoes":
+		RotaController::versoes();
+		break;
+	default:
+		// Exibe um autor em específico
+		if(in_array(strtoupper($direcionador), $autores)) {
+			RotaController::autor($direcionador);
+		}
+		elseif(in_array(strtoupper($direcionador), $livros)) {
+			// Exibe um versículo específico
+			if($versiculo !== 0) {
+				RotaController::versiculo($versao, strtoupper($direcionador), $capitulo, $versiculo);
+			}
+			// Exibe um capítulo específico
+			elseif($capitulo !== 0) {
+				RotaController::capitulo(strtoupper($direcionador), $versao, $capitulo);
+			}
+			// Exibe um livro específico
+			else {
+				RotaController::livro(strtoupper($direcionador));
+			}
+		}
+		else {
+			if(Operador::logged()) {
+				RotaController::versao($versao);
+			}
+			else {
+				RotaController::home();
+			}
+		}
 		break;
 }
